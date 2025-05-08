592cbe519e514d1c77e6adc35f0c3a97dfe82305
khandelwalkunal779 <86685851+khandelwalkunal779@users.noreply.github.com>
2025-05-09 01:38:27 +0530

    Added scrollBtn and DownloadBtn

---
+++ assets/js/scrollbutton.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {
+    const scrollBtn = document.getElementById("scrollBtn");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+};
