885e9cb219a74caa4974fd0ce1c7cad244f9f3e4
khandelwalkunal779 <86685851+khandelwalkunal779@users.noreply.github.com>
2025-05-11 02:53:18 +0530

    Added college campus groups

--- assets/js/showmore.js
+++ assets/js/showmore.js
@@ -1,6 +1,16 @@
-function toggleSection() {
-    const section = document.getElementById('hidden-section');
-    const button = document.getElementById('showmore-btn');
+function toggleExperiences() {
+    const section = document.getElementById('exp-section');
+    const button = document.getElementById('showmore-exp-btn');
+
+    section.classList.remove('hidden');
+    section.classList.add('fade-in');
+
+    button.remove();
+}
+
+function toggleProjects() {
+    const section = document.getElementById('projects-section');
+    const button = document.getElementById('showmore-projects-btn');
 
     section.classList.remove('hidden');
     section.classList.add('fade-in');
